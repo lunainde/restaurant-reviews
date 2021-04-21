bb8ba08b7339416202d6ceebff995cd9606e7f20
Diana Ka <75699816+lunainde@users.noreply.github.com>
2021-04-21 23:32:06 +0200

    bootstrap/model/100seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+# dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", stars: 5 }
+# pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", stars: 4 }
+
+# [ dishoom, pizza_east ].each do |attributes|
+#   restaurant = Restaurant.create!(attributes)
+#   puts "Created #{restaurant.name}"
+# end
+
+100.times do
+    Restaurant.create(
+        name: Faker::Restaurant.name,
+        address: Faker::Address.street_address,
+        stars: rand(1..5)
+        )
+   end
+
+puts "Finished!"
